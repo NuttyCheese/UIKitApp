f6cbbe868d667ee4aa8d2d0bcdae00ff7f010b97
BorisPavlov <pbd_b@mail.ru>
2022-04-21 14:52:41 +0300

    UIKit info

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -9,11 +9,90 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var datePicket: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Label
+        
+        mainLabel.font = mainLabel.font.withSize(30)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        mainLabel.textColor = .white
+        
+        //segmentedControl
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        //slider
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
+        mainLabel.text = String(slider.value)
+        
+        //Date picker
+        datePicket.locale = Locale(identifier: "ru_Ru")
+        
     }
 
+    @IBAction func segmentedControlAction() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segment is selected"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The second segment is selected"
+            mainLabel.textColor = .blue
+        default:
+            mainLabel.text = "The third segment is selected"
+            mainLabel.textColor = .yellow
+        }
+    }
+    
+    @IBAction func sliderAction() {
+        mainLabel.text = String(slider.value)
+        let sliderValue = CGFloat(slider.value)
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(sliderValue)
+        
+    }
+    
+    @IBAction func doneButtonPressed() {
+        guard let inputText = textField.text, !inputText.isEmpty else {
+            showAlert(with: "Text field is empty", and: "Please enter your name")
+            return
+        }
+        if let _ = Double(inputText) {
+            showAlert(with: "Wrong format", and: "Please enter your name")
+            return
+        }
+        mainLabel.text = textField.text
+    }
+    
+    @IBAction func datePickerAction() {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.locale = Locale(identifier: "ru_RU")
+        
+        mainLabel.text = formatter.string(from: datePicket.date)
+    }
+    
+}
 
+//MARK: - Private Methods
+extension ViewController {
+    private func showAlert(with title: String, and message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "Ok", style: .default) { _ in
+            self.textField.text = ""
+        }
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
 }
 
